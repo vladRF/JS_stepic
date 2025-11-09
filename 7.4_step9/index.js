2f3415350dd6bab6d1051cb762def65c095e9c5f
Vladislav Bologov <wlad.bologov@gmail.com>
2025-11-09 21:41:48 +0300

    added task 7.4_step9

---
+++ 7.4_step9/index.js
@@ -0,0 +1,9 @@
+let userInput = -81;
+
+if (userInput < 0) {
+    console.log(`Квадратный корень из ${Math.abs(userInput)} равен ${Math.sqrt(Math.abs(userInput))}`)
+
+} else {
+    console.log(`Квадратный корень из ${Math.abs(userInput)} равен ${Math.sqrt(userInput)}`)
+
+}
